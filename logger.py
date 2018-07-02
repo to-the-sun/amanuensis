fbc8604a332c32ea4980aab4dcb213b1b785381e
Ajmal Noushad <ajufhn@gmail.com>
2018-07-03 03:23:13 +0530

    Receive from both ports

--- logger.py
+++ logger.py
@@ -1,17 +1,24 @@
 import socket
+from datetime import datetime
 
-logfile = open("log.txt", "w")
+logfile = open("logs/Log {}.txt".format(datetime.now().strftime("%m-%d-%y%H%M%S")), "w")
 
 logger = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-logger.bind(("127.0.0.1", 10248))
+logger.bind(("127.0.0.1", 10247))
 
 message = ""
 
-while message != "close":
-    message, clientAddress = logger.recvfrom(10249)
-    print(message + "\n")
-    logfile.write(message + "\n")
+byte_string = b''
 
-logger.close()
-logfile.close()
+try:
+    while "close" not in message:
+        byte_string = logger.recv(128)
+        message = byte_string.decode('utf-8')
+        print(message)
+        logfile.write(str(datetime.now()) + ' ' + message + "\n")
+except Exception as e:
+    print(e)
+finally:
+    logger.close()
+    logfile.close()
