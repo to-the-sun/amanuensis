f4a485f69ab0a20b04a885e8e74e6c2a5030993a
Ajmal Noushad <ajufhn@gmail.com>
2018-07-04 04:59:18 +0530

    Change log directory on projectPath message

--- logger.py
+++ logger.py
@@ -1,22 +1,34 @@
 import socket
-from datetime import datetime
-
-logfile = open("logs/Log {}.txt".format(datetime.now().strftime("%m-%d-%y%H%M%S")), "w")
+from shutil import copyfile
 
 logger = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 logger.bind(("127.0.0.1", 10247))
+
 
 message = ""
 
-byte_string = b''
+# Initializing log directory and file
+projectPath = ""
+log_name = "log.txt"
+logfile = open(log_name, "w")
+
 
 try:
     while "close" not in message:
-        byte_string = logger.recv(128)
-        message = byte_string.decode('utf-8')
+        message = bytes.decode(logger.recv(128))
         print(message)
-        logfile.write(str(datetime.now()) + ' ' + message + "\n")
+        logfile.write(message + "\n")
+        message = message.split(' ')
+        if message[0] == 'projectPath':
+            logfile.close()
+            if projectPath == "":
+                projectPath = message[1] + '\\' + log_name
+                copyfile(log_name, projectPath)
+            else:
+                newProjectPath = message[1] + '\\' + log_name
+                copyfile(projectPath, newProjectPath)
+                projectPath = newProjectPath
+            logfile = open(projectPath, "a")
 except Exception as e:
     print(e)
 finally:
