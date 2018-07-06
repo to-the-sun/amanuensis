001858c1ad3a2849ec9ff1c382b99bdcd38321b3
Ajmal Noushad <ajufhn@gmail.com>
2018-07-06 09:46:07 +0530

    Accept generic projectPaths

--- logger.py
+++ logger.py
@@ -1,5 +1,6 @@
 import socket
 from shutil import copyfile
+from pathlib import Path, PureWindowsPath
 
 logger = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 logger.bind(("127.0.0.1", 10247))
@@ -8,27 +9,27 @@ logger.bind(("127.0.0.1", 10247))
 message = ""
 
 # Initializing log directory and file
-projectPath = ""
+projectPath = Path("")
 log_name = "log.txt"
 logfile = open(log_name, "w")
 
 
 try:
     while "close" not in message:
-        message = bytes.decode(logger.recv(128))
+        message = bytes.decode(logger.recv(99999))
         print(message)
         logfile.write(message + "\n")
         message = message.split(' ')
         if message[0] == 'projectPath':
             logfile.close()
-            if projectPath == "":
-                projectPath = message[1] + '\\' + log_name
-                copyfile(log_name, projectPath)
+            if projectPath == Path(""):
+                projectPath = Path(message[1]) / log_name
+                copyfile(log_name, str(projectPath))
             else:
-                newProjectPath = message[1] + '\\' + log_name
-                copyfile(projectPath, newProjectPath)
+                newProjectPath = Path(message[1]) / log_name
+                copyfile(str(projectPath), str(newProjectPath))
                 projectPath = newProjectPath
-            logfile = open(projectPath, "a")
+            logfile = projectPath.open("a")
 except Exception as e:
     print(e)
 finally:
